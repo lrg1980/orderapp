304f3a6c3f3135d9f6aedf841b4a0d2cd58cb433
Leonardo Grabow <admin@binweb.net>
2017-12-26 21:05:32 -0300

    Proyecto Base

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Escritorio</div>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    Ya estas dentro de OrderApp!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
